bcbdfe29bbdf07e7d41d076feda0351dd9977d46
Nellie K <noircir@users.noreply.github.com>
2019-01-28 22:34:11 -0500

    refactor to async/await and add proxy to front end

--- services/passport.js
+++ services/passport.js
@@ -26,16 +26,15 @@ passport.use(
             // trust the non-https:// routes
             proxy: true
         },
-        (accessToken, refreshToken, profile, done) => {
-            User.findOne({ googleId: profile.id }).then((existingUser) => {
-                if (existingUser) {
-                    done(null, existingUser);
-                } else {
-                    new User({ googleId: profile.id })
-                        .save()
-                        .then(user => done(null, user));
-                }
-            });
+        async (accessToken, refreshToken, profile, done) => {
+            const existingUser = await User.findOne({ googleId: profile.id })
+            
+            if (existingUser) {
+                return done(null, existingUser);
+            } 
+            
+            const user = await new User({ googleId: profile.id }).save()
+            done(null, user);
         }
     )
 );

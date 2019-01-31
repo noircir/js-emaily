432e2173910f4a956ffa6e9ce279f0d963087f14
Nellie K <noircir@users.noreply.github.com>
2019-01-30 19:13:13 -0500

    wire up the Emaily navigation logo

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Landing = () => {
     return (
-        <div>
+        <div style={{ textAlign: 'center' }}>
             <h1>Emaily!</h1>
             Collect feedback from your users
         </div>
     )
 }
+
+export default Landing;

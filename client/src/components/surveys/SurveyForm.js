2e306471dbefa63c431e2d2d933478ad154213fa
Nellie <7676ee@gmail.com>
2019-02-12 23:51:42 -0500

    redux form validation function

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { reduxForm, Field } from 'redux-form';
 import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
+import validateEmails from '../../utils/validateEmails';
 
 const FIELDS = [
 	{ label: 'Survey Title', name: 'title', noValueError: 'Provide a survey title' },
@@ -39,11 +40,14 @@ class SurveyForm extends React.Component {
 function validate(values) {
 	const errors = {};
 
+	errors.emails = validateEmails(values.emails || '');
+
 	_.each(FIELDS, ({ name, noValueError }) => {
 		if (!values[name]) {
 			errors[name] = noValueError;
 		}
 	});
+
 	return errors;
 }
 

fb56095ab4f006b33909ce36b3d6cccafdf7a322
Nellie <elenagithub@gmail.com>
2019-02-15 22:55:28 -0500

    add withRouter to SurveyFormReview to keep track of history

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,10 +1,11 @@
 import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import formFields from './formFields';
 import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
 	const reviewFields = _.map(formFields, ({ label, name }) => {
 		return (
 			<div key={name}>
@@ -21,7 +22,7 @@ const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
 			<button className="yellow darken-2 btn-flat white-text" onClick={onCancel}>
 				Back
 			</button>
-			<button onClick={() => submitSurvey(formValues)} className="green btn-flat right white-text">
+			<button onClick={() => submitSurvey(formValues, history)} className="green btn-flat right white-text">
 				Send Survey
 				<i className="material-icons right">email</i>
 			</button>
@@ -34,4 +35,4 @@ const mapStateToProps = (state) => {
 	return { formValues: state.form.surveyForm.values };
 };
 
-export default connect(mapStateToProps, actions)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));

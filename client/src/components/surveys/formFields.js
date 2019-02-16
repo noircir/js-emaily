d8e1c9bd4ff597c521981f10fddfd6ea49d4dd72
Nellie <elenagithub@gmail.com>
2019-02-15 22:21:54 -0500

    add submitSurvey action

--- client/src/components/surveys/formFields.js
+++ client/src/components/surveys/formFields.js
@@ -2,5 +2,5 @@ export default [
 	{ label: 'Survey Title', name: 'title', noValueError: 'Provide a survey title' },
 	{ label: 'Subject Line', name: 'subject', noValueError: 'Provide a subject line' },
 	{ label: 'Email Body', name: 'body', noValueError: 'Provide an email body' },
-	{ label: 'Recipient List', name: 'emails', noValueError: 'Provide a list of recipients' }
+	{ label: 'Recipient List', name: 'recipients', noValueError: 'Provide a list of recipients' }
 ];

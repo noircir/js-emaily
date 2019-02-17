bf8810adf862e4c0c12349c8c2659c471072839f
Nellie <elenagithub@gmail.com>
2019-02-17 18:35:47 -0500

    blacklist recipients in Mongoose query

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,3 @@
-
 import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -13,13 +12,14 @@ import reducers from './reducers';
 //  const mySurvey = {subject: '..', title: '..', recipients: '..', body: '..'}
 //  axios.post(mySurvey, survey);
 
-// import axios from 'axios';
-// window.axios = axios;
+import axios from 'axios';
+window.axios = axios;
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, 
-    document.querySelector('#root'));
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.querySelector('#root')
+);
--- index.js
+++ index.js
@@ -9,18 +9,19 @@ require('./models/Survey');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
-mongoose.set("debug", true);
+mongoose.set('debug', true);
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use(cookieSession({
-    keys: [keys.cookieKey],
+app.use(
+	cookieSession({
+		keys: [ keys.cookieKey ],
 
-    // Cookie Options
-    maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
-    })
+		// Cookie Options
+		maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
+	})
 );
 
 app.use(passport.initialize());
@@ -31,15 +32,15 @@ require('./routes/billingRoutes')(app);
 require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
-    // Express will serve up production assets like main.js file, or main.css file
-    app.use(express.static('client/build'));
+	// Express will serve up production assets like main.js file, or main.css file
+	app.use(express.static('client/build'));
 
-    // The catch-all case, when no routes match:
-    // Express will serve up the index.html file if it doesn't recognize the route. 
-    const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
+	// The catch-all case, when no routes match:
+	// Express will serve up the index.html file if it doesn't recognize the route.
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
 }
 
 const PORT = process.env.PORT || 5000;
